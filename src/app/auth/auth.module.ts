b1aa8cb97e353b2a5f3b8f7df31db9fdb20fa03c
LUIS-VILLALBA\User <luisvillalba244@gmail.com>
2024-08-03 12:40:00 -0500

    Add validators, and fixes to the Login Form component, change favicon, add initial user-home component.

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 import LoginFormComponent from './login-form/login-form.component';
 
 @NgModule({
@@ -9,7 +10,8 @@ import LoginFormComponent from './login-form/login-form.component';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule
   ],
   exports: [
     LoginFormComponent
